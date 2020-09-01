0e78486422153fc6b755189b9c3842f27a190804
Xin Yu Wei <davidsajare@users.noreply.github.com>
2020-09-01 15:19:24 +0800

    Create pv.sh

---
+++ OpenShift/pv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SERVER=`hostname`
+COUNT=70
+
+sudo mkdir -p /exports
+sudo chmod 777 /exports
+sudo chown nfsnobody:nfsnobody /exports
+oc project default
+
+for i in $(seq 1 $COUNT); do
+    PV=$(cat <<EOF
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: pv$(printf %04d $i)
+spec:
+  capacity:
+    storage: 5Gi
+  accessModes:
+    - ReadWriteMany
+  persistentVolumeReclaimPolicy: Recycle
+  nfs:
+    server: $SERVER
+    path: /exports/pv$(printf %04d $i)
+EOF
+)
+    echo "$PV" | oc create -f -
+    sudo mkdir -p /exports/pv$(printf %04d $i)
+    sudo chown nfsnobody:nfsnobody /exports/pv$(printf %04d $i)
+done
